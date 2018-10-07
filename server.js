d3a1eb3a72328e41c0c8dfca65ea4090cdc09c33
zaynab22 <43920434+zaynab22@users.noreply.github.com>
2018-10-08 01:19:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use('/', routes);
 app.use('/api', apiRoutes);
  
 // setting port number for running server
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
  
 // starting express server
 app.listen(port, function() {
