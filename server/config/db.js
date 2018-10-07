94b25fb614c7651c25892e62a3e6efb8e6661e6b
zaynab22 <43920434+zaynab22@users.noreply.github.com>
2018-10-08 01:02:00 +0200

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var connection = mongoose.connect('mongodb://localhost/mean_db');
+var connection = mongoose.connect('mongodb://zaynab:zaynab22@ds125453.mlab.com:25453/myapp');
  
 module.exports = connection;
 
